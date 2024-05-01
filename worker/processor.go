cc175e10b0886a69b9c809b4e0c0cc63d305fa78
cheng <1240800450@qq.com>
2024-05-01 15:43:26 +0800

    feat: Send async tasks to Redis within a DB transaction

--- worker/processor.go
+++ worker/processor.go
@@ -7,6 +7,8 @@ import (
 	db "simplebank/db/sqlc"
 
 	"github.com/hibiken/asynq"
+	"github.com/redis/go-redis/v9"
+	"github.com/rs/zerolog/log"
 )
 
 const (
@@ -25,13 +27,20 @@ type RedisTaskProcessor struct {
 }
 
 func NewRedisTaskProcessor(redisOpt asynq.RedisClientOpt, store db.Store) TaskProcessor {
+	logger := NewLogger()
+	redis.SetLogger(logger)
 	server := asynq.NewServer(
-		redisOpt, 
+		redisOpt,
 		asynq.Config{
-			Queues: map[string]int {
+			Queues: map[string]int{
 				QueueCritical: 10,
 				QueueDefault:  5,
 			},
+			ErrorHandler: asynq.ErrorHandlerFunc(func(ctx context.Context, task *asynq.Task, err error) {
+				log.Error().Err(err).Str("type", task.Type()).
+					Bytes("payload", task.Payload()).Msg("processing task failed")
+			}),
+			Logger: logger,
 		},
 	)
 
